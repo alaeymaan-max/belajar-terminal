2ca2c90e54bcc136de919f3f5ce5ca5916280d4e
Muhammad Aiman bin Mohamad Rafee <aiman_96@RafeeHP.localdomain>
2026-02-07 14:52:01 +0800

    Misi OS: Program C pertama berjaya dicipta

---
+++ README.md
@@ -0,0 +1,4 @@
+# Perjalanan Aiman Menuju OS Creator
+## Step 1: Terminal Mastery
+- [x] Cipta akaun user baru (aiman_96)
+- [x] Beri kuasa sudo kepada user
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Aiman OS: Memulakan sistem...\n");
+    printf("Hello World dari Terminal WSL!\n");
+    return 0;
+}
